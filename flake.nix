12c2e819efc4a3de672133ccee5259cf3934880b
aiverson <alexjiverson@gmail.com>
2023-05-05 06:53:33 -0700

    add a flake so I can use a devshell

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description =
+    "An experimental language for high performance safe convenient metaprogramming";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    luvitpkgs = {
+      url = "github:aiverson/luvit-nix";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, luvitpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = rec {
+          hello = pkgs.hello;
+          default = hello;
+        };
+        apps = rec {
+          hello =
+            flake-utils.lib.mkApp { drv = self.packages.${system}.hello; };
+          default = hello;
+        };
+        devShells = rec {
+          alicorn = pkgs.mkShell {
+            buildInputs = [
+              luvitpkgs.packages.${system}.lit
+              luvitpkgs.packages.${system}.luvit
+            ];
+          };
+          default = alicorn;
+        };
+      });
+}
