8fc8f921349330c8816128929cc1b7f5c6670c11
bootstrap-prime <bootstrap.prime@gmail.com>
2023-07-03 13:44:54 +0000

    feat: reworked formatter

--- format-adapter.lua
+++ format-adapter.lua
@@ -1,34 +1,21 @@
 local metalanguage = require 'metalanguage'
-local format = require 'temp-format'
+local format = require 'format'
 local types = require './typesystem'
 
-local anchor_mt = {
-  __tostring = function(self)
-    return "in file" .. self.sourceid .. ", line " .. self.line .. " character " .. self.char
-  end
-}
-
-local function anchor(anchor) 
-  if anchor then
-    return setmetatable(anchor, anchor_mt)
-  else
-    return anchor
-  end
-end
 
 local function syntax_convert(tree)
   if tree.kind == "list" then
     local res = metalanguage.nilval
     for i = #tree.elements, 1, -1 do
-      res = metalanguage.pair(anchor(tree.anchor), syntax_convert(tree.elements[i]), res)
+      res = metalanguage.pair(tree.anchor, syntax_convert(tree.elements[i]), res)
     end
     return res
   elseif tree.kind == "symbol" then
-    return metalanguage.symbol(anchor(tree.anchor), tree.str)
+    return metalanguage.symbol(tree.anchor, tree.str)
   elseif tree.kind == "literal" then
     if tree.literaltype == "f64" then
       -- metalanguage.value use here is only correct for smoketest language and will need changed in future
-      return metalanguage.value(anchor(tree.anchor), {type = types.number, val = tree.val})
+      return metalanguage.value(tree.anchor, {type = types.number, val = tree.val})
     else
       error "syntax contains a literal of a type other than the basic number"
     end
