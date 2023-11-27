aef789a040c4b8c90a049446bf904df25415d540
AstroSnail <astrosnail@protonmail.com>
2023-11-27 09:06:25 +0000

    Revert commenting out all the prints
    hopefully this means the merge will be easier

--- evaluator.lua
+++ evaluator.lua
@@ -281,19 +281,6 @@ function fitsinto(a, b)
 	if a == b then
 		return true
 	end
-	--[[
-	print "AAAAAAAAAAAAAAA (evaluator.lua)"
-	if a and a.pretty_print then
-		print(a:pretty_print())
-	else
-		print(type(a))
-	end
-	if b and b.pretty_print then
-		print(b:pretty_print())
-	else
-		print(type(b))
-	end
-	--]]
 	return value_fitsinto(a, b)
 end
 
@@ -406,19 +393,14 @@ local function check(
 		error("check, target_type: expected a target type (as an alicorn value)")
 	end
 
-	--print("!!!!!!!!!!!!! running check!")
-	--print(checkable_term:pretty_print())
-	--print(target_type:pretty_print())
-	--print("///////////// running check!")
-
 	if checkable_term:is_inferrable() then
 		local inferrable_term = checkable_term:unwrap_inferrable()
 		local inferred_type, inferred_usages, typed_term = infer(inferrable_term, typechecking_context)
 		-- TODO: unify!!!!
 		if inferred_type ~= target_type then
-			--print "attempting to check if terms fit"
-			--print(inferred_type:pretty_print())
-			--print(target_type:pretty_print())
+			print "attempting to check if terms fit"
+			print(inferred_type:pretty_print())
+			print(target_type:pretty_print())
 			local ok, err = fitsinto(inferred_type, target_type)
 			if not ok then
 				--inferred_type = inferred_type:unify(target_type)
@@ -1053,10 +1035,6 @@ function infer(
 		return bodytype, result_usages, terms.typed_term.let(exprterm, bodyterm)
 	elseif inferrable_term:is_prim_intrinsic() then
 		local source, type = inferrable_term:unwrap_prim_intrinsic()
-		print("!!!!!!!!!!!!! check out what we got!")
-		print(source:pretty_print())
-		print(type:pretty_print())
-		print("///////////// check out what we got!")
 		local source_type, source_usages, source_term = check(source, typechecking_context, unrestricted(value.prim_string_type))
 		local type_type, type_usages, type_term = check(type, typechecking_context, value.qtype_type(0))
 		-- FIXME: type_type, source_type are ignored, need checked?
