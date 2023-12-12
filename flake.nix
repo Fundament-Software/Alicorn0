c7369baea1a96227e49c3e5423e4d5d949f0feb9
Luna Nova <git@lunnova.dev>
2023-12-12 00:05:59 -0800

    flake: set up pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -3,16 +3,21 @@
     "An experimental language for high performance safe convenient metaprogramming";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     luvitpkgs = {
       url = "github:aiverson/luvit-nix";
       # inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, luvitpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, luvitpkgs, flake-utils, pre-commit-hooks }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -28,7 +33,7 @@
       in
       {
         packages = rec {
-          hello = pkgs.hello;
+          inherit (pkgs) hello;
           default = hello;
         };
         apps = rec {
@@ -43,6 +48,15 @@
             mkdir $out
             ${pkgs.lib.getExe pkgs-unstable.stylua} . -c
           '';
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              statix.enable = true;
+              nixpkgs-fmt.enable = true;
+              stylua.enable = true;
+              deadnix.enable = true;
+            };
+          };
         };
         # nix fmt's docs say this should only be for *.nix files but we're ignoring that as that's inconvenient
         # See https://github.com/NixOS/nix/issues/9132#issuecomment-1754999829
@@ -65,6 +79,7 @@
               (pkgs.luajit.withPackages
                 (ps: with ps; [ luasocket lpeg inspect luaunit tl ]))
             ];
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
           };
           default = alicorn;
         };
