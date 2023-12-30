be3b61a4df067c0b7244f34d7e445209fa0965e3
aiverson <alexjiverson@gmail.com>
2023-12-29 19:55:53 -0800

    make fibonacci buffer more tolerant of nils

--- fibonacci-buffer.lua
+++ fibonacci-buffer.lua
@@ -25,7 +25,7 @@ function FibonacciBuffer:append(value)
 	local merge_from = n_partitions
 	local merge_length = 1
 	for i = n_partitions - 1, 1, -1 do
-		local partition_length = #self[i]
+		local partition_length = self[i].n
 		if partition_length == merge_length then
 			merge_from = merge_from - 1
 			merge_length = merge_length * 2
@@ -47,13 +47,14 @@ function FibonacciBuffer:append(value)
 			table.move(self[i], 1, partition_length, merged_length + 1, merged_partition)
 			merged_length = merged_length + partition_length
 		end
+		merged_partition.n = merged_length
 	end
 	-- now we build the output
 	local fib_buf = new()
 	-- reference old partitions that didn't need to be merged
 	table.move(self, 1, merge_from - 1, 1, fib_buf)
 	fib_buf[merge_from] = merged_partition
-	fib_buf[merge_from + 1] = { value }
+	fib_buf[merge_from + 1] = { value, n = 1 }
 	fib_buf.n = n + 1
 	return fib_buf
 end
@@ -61,7 +62,7 @@ end
 -- one-based!!!
 function FibonacciBuffer:get(index)
 	for _, p in ipairs(self) do
-		local length = #p
+		local length = p.n
 		if index <= length then
 			return p[index]
 		else
@@ -78,8 +79,13 @@ end
 function FibonacciBuffer:debug_repr()
 	local partition_strings = {}
 	for i, p in ipairs(self) do
-		partition_strings[i] =
-			string.format('[ partition=%u table="%s" length=%u data={ %s } ]', i, tostring(p), #p, table.concat(p, " "))
+		partition_strings[i] = string.format(
+			'[ partition=%u table="%s" length=%u data={ %s } ]',
+			i,
+			tostring(p),
+			p.n,
+			table.concat(p, " ", 1, p.n)
+		)
 	end
 	local output =
 		string.format('table="%s" n_partitions=%u %s', tostring(self), #self, table.concat(partition_strings, " "))
