5847c7f554d30d6cbda203125ad81706c019fa68
AstroSnail <astrosnail@protonmail.com>
2024-06-04 10:37:11 +0000

    Add a better profiler

--- runtest.lua
+++ runtest.lua
@@ -76,14 +76,75 @@ if print_inferrable then
 end
 
 print("Inferring")
-local profiler = nil
+local profile, profile_n, profile_samples
 if profile_run then
-	profiler = require("jit.p")
-	profiler.start("G", profile_file)
+	profile = require("jit.profile")
+	profile_n = 0
+	profile_samples = {}
+	profile.start("li100", function(thread, samples, vmstate)
+		local stack_n = 0
+		local stack = {}
+		-- luajit's profiler loops from 0 to 100
+		-- hopefully 1000 will be enough for us for a while
+		for i = 0, 1000 do
+			local d = debug.getinfo(thread, i, "Sfln")
+			if not d then
+				break
+			end
+			local infer = d.func == evaluator.infer
+			local check = d.func == evaluator.check
+			local term, context, goal
+			if infer or check then
+				_, term = debug.getlocal(thread, i, 1)
+				_, context = debug.getlocal(thread, i, 2)
+			end
+			if check then
+				_, goal = debug.getlocal(thread, i, 3)
+			end
+			stack_n = stack_n + 1
+			stack[stack_n] = {
+				i = i,
+				name = d.name,
+				namewhat = d.namewhat,
+				source = d.source,
+				short_src = d.short_src,
+				linedefined = d.linedefined,
+				lastlinedefined = d.lastlinedefined,
+				what = d.what,
+				currentline = d.currentline,
+				func = d.func,
+				infer = infer,
+				check = check,
+				term = term,
+				context = context,
+				goal = goal,
+			}
+		end
+		profile_n = profile_n + 1
+		profile_samples[profile_n] = {
+			stack_n = stack_n,
+			stack = stack,
+			samples = samples,
+			vmstate = vmstate,
+		}
+	end)
 end
 local type, usages, term = evaluator.infer(bound_expr, terms.typechecking_context())
 if profile_run then
-	profiler.stop()
+	profile.stop()
+	local profile_out = io.open(profile_file, "w")
+	for n, sample in ipairs(profile_samples) do
+		local s = sample.stack
+		for i = sample.stack_n, 1, -1 do
+			local d = s[i]
+			profile_out:write(string.format("%s %s %s: %s:%d", d.what, d.namewhat, d.name, d.source, d.currentline))
+			if i > 1 then
+				profile_out:write(";")
+			end
+		end
+		profile_out:write(" ", sample.samples, "\n")
+	end
+	profile_out:close()
 end
 print("Inferred!")
 if print_typed then
