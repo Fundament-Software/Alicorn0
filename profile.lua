fb730f31f8b14b824d754faea226f43eacdf91de
AstroSnail <astrosnail@protonmail.com>
2024-06-11 00:27:38 +0000

    Factor profiler to a separate file

---
+++ profile.lua
@@ -0,0 +1,111 @@
+local evaluator = require "./evaluator"
+local profile, profile_n, profile_samples
+
+local function start()
+	profile = require("jit.profile")
+	profile_n = 0
+	profile_samples = {}
+	profile.start("li100", function(thread, samples, vmstate)
+		local stack_n = 0
+		local stack = {}
+		-- luajit's profiler loops from 0 to 100
+		-- hopefully 1000 will be enough for us for a while
+		for i = 0, 1000 do
+			local d = debug.getinfo(thread, i, "Sfln")
+			if not d then
+				break
+			end
+			local infer = d.func == evaluator.infer
+			local check = d.func == evaluator.check
+			local term, context, goal, inferred_type
+			if infer or check then
+				_, term = debug.getlocal(thread, i, 1)
+				_, context = debug.getlocal(thread, i, 2)
+			end
+			if check then
+				_, goal = debug.getlocal(thread, i, 3)
+				_, inferred_type = debug.getlocal(thread, i, 5)
+			end
+			stack_n = stack_n + 1
+			stack[stack_n] = {
+				i = i,
+				name = d.name,
+				namewhat = d.namewhat,
+				source = d.source,
+				short_src = d.short_src,
+				linedefined = d.linedefined,
+				lastlinedefined = d.lastlinedefined,
+				what = d.what,
+				currentline = d.currentline,
+				func = d.func,
+				infer = infer,
+				check = check,
+				term = term,
+				context = context,
+				goal = goal,
+				inferred_type = inferred_type,
+			}
+		end
+		profile_n = profile_n + 1
+		profile_samples[profile_n] = {
+			stack_n = stack_n,
+			stack = stack,
+			samples = samples,
+			vmstate = vmstate,
+		}
+	end)
+end
+
+local function stop()
+	profile.stop()
+end
+
+local function dump(profile_file)
+	local profile_out = io.open(profile_file, "w")
+	for n, sample in ipairs(profile_samples) do
+		profile_out:write("sample: ", n, "\n")
+		local t = false
+		for _, d in ipairs(sample.stack) do
+			local depth = sample.stack_n - d.i
+			profile_out:write(
+				string.format("%d %s %s %s: %s:%d\n", depth, d.what, d.namewhat, d.name, d.source, d.currentline)
+			)
+			if not t and (d.infer or d.check) then
+				profile_out:write(d.context:format_names())
+				profile_out:write(d.term:pretty_print(d.context), "\n")
+				profile_out:write(d.term:default_print(), "\n")
+				if d.check then
+					profile_out:write(d.goal:pretty_print(d.context), "\n")
+					profile_out:write(d.goal:default_print(), "\n")
+					profile_out:write(d.inferred_type:pretty_print(d.context), "\n")
+					profile_out:write(d.inferred_type:default_print(), "\n")
+				end
+				t = true
+			end
+		end
+	end
+	profile_out:close()
+end
+
+local function dump_flame(profile_file)
+	local profile_out = io.open(profile_file, "w")
+	for n, sample in ipairs(profile_samples) do
+		local s = sample.stack
+		for i = sample.stack_n, 1, -1 do
+			local d = s[i]
+			profile_out:write(string.format("%s %s %s: %s:%d", d.what, d.namewhat, d.name, d.source, d.currentline))
+			if i > 1 then
+				profile_out:write(";")
+			end
+		end
+		profile_out:write(" ", sample.samples, "\n")
+	end
+	profile_out:close()
+end
+
+return {
+	start = start,
+	stop = stop,
+	dump = dump,
+	dump_flame = dump_flame,
+}
--- runtest.lua
+++ runtest.lua
@@ -5,6 +5,7 @@ local base_env = require "./base-env"
 local p = require "pretty-print".prettyPrint
 local terms = require "./terms"
 local exprs = require "./alicorn-expressions"
+local profile = require "./profile"
 local fs = require "fs"
 local path = require "path"
 
@@ -83,100 +84,17 @@ if print_inferrable then
 end
 
 print("Inferring")
-local profile, profile_n, profile_samples
 if profile_run then
-	profile = require("jit.profile")
-	profile_n = 0
-	profile_samples = {}
-	profile.start("li100", function(thread, samples, vmstate)
-		local stack_n = 0
-		local stack = {}
-		-- luajit's profiler loops from 0 to 100
-		-- hopefully 1000 will be enough for us for a while
-		for i = 0, 1000 do
-			local d = debug.getinfo(thread, i, "Sfln")
-			if not d then
-				break
-			end
-			local infer = d.func == evaluator.infer
-			local check = d.func == evaluator.check
-			local term, context, goal, inferred_type
-			if infer or check then
-				_, term = debug.getlocal(thread, i, 1)
-				_, context = debug.getlocal(thread, i, 2)
-			end
-			if check then
-				_, goal = debug.getlocal(thread, i, 3)
-				_, inferred_type = debug.getlocal(thread, i, 5)
-			end
-			stack_n = stack_n + 1
-			stack[stack_n] = {
-				i = i,
-				name = d.name,
-				namewhat = d.namewhat,
-				source = d.source,
-				short_src = d.short_src,
-				linedefined = d.linedefined,
-				lastlinedefined = d.lastlinedefined,
-				what = d.what,
-				currentline = d.currentline,
-				func = d.func,
-				infer = infer,
-				check = check,
-				term = term,
-				context = context,
-				goal = goal,
-				inferred_type = inferred_type,
-			}
-		end
-		profile_n = profile_n + 1
-		profile_samples[profile_n] = {
-			stack_n = stack_n,
-			stack = stack,
-			samples = samples,
-			vmstate = vmstate,
-		}
-	end)
+	profile.start()
 end
 local type, usages, term = evaluator.infer(bound_expr, terms.typechecking_context())
 if profile_run then
 	profile.stop()
-	local profile_out = io.open(profile_file, "w")
-	for n, sample in ipairs(profile_samples) do
-		if profile_flame then
-			local s = sample.stack
-			for i = sample.stack_n, 1, -1 do
-				local d = s[i]
-				profile_out:write(string.format("%s %s %s: %s:%d", d.what, d.namewhat, d.name, d.source, d.currentline))
-				if i > 1 then
-					profile_out:write(";")
-				end
-			end
-			profile_out:write(" ", sample.samples, "\n")
-		else
-			profile_out:write("sample: ", n, "\n")
-			local t = false
-			for _, d in ipairs(sample.stack) do
-				local depth = sample.stack_n - d.i
-				profile_out:write(
-					string.format("%d %s %s %s: %s:%d\n", depth, d.what, d.namewhat, d.name, d.source, d.currentline)
-				)
-				if not t and (d.infer or d.check) then
-					profile_out:write(d.context:format_names())
-					profile_out:write(d.term:pretty_print(d.context), "\n")
-					profile_out:write(d.term:default_print(), "\n")
-					if d.check then
-						profile_out:write(d.goal:pretty_print(d.context), "\n")
-						profile_out:write(d.goal:default_print(), "\n")
-						profile_out:write(d.inferred_type:pretty_print(d.context), "\n")
-						profile_out:write(d.inferred_type:default_print(), "\n")
-					end
-					t = true
-				end
-			end
-		end
+	if profile_flame then
+		profile.dump_flame(profile_file)
+	else
+		profile.dump(profile_file)
 	end
-	profile_out:close()
 end
 print("Inferred!")
 if print_typed then
