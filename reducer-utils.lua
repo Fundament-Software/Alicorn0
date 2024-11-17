003764ade79c6c52c0614c29995da39dfdf2c8e6
AstroSnail <astrosnail@protonmail.com>
2024-11-17 02:39:03 +0000

    Replace { ... } and select("#", ...) with table.pack(...)
    and associated refactors
    
    Excuse me, sir, do you have a moment to talk about our lord and savior,
    table.pack?

--- reducer-utils.lua
+++ reducer-utils.lua
@@ -5,11 +5,15 @@ local function unpack_val_env(val_and_env)
 	return val_and_env.val, val_and_env.env
 end
 local function accept_bundled(data, ...)
-	return true, { ... }
+	return true, table.pack(...)
+end
+local function unpack_bundle(bun)
+	return table.unpack(bun, 1, bun.n)
 end
 
 return {
 	accept_with_env = accept_with_env,
 	unpack_val_env = unpack_val_env,
 	accept_bundled = accept_bundled,
+	unpack_bundle = unpack_bundle,
 }
