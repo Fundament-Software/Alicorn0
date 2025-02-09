Fundament-Software/Alicorn0

commit 67e3cfef931657027dfc922119ac23dbfd42ff1d

don't tail call by default (part two)

terms-gen-meta.lua
@@ -109,7 +109,7 @@ local function build_meta_file_for_record(info)
 		variant = kind,
 		parts = unwraptypes,
 	}
-	return meta_gen {
+	return U.notail(meta_gen({
 		kind = "file",
 		filename = "types." .. kind,
 		definition = {
@@ -119,7 +119,7 @@ local function build_meta_file_for_record(info)
 			methods = methods,
 			constructor = constructor,
 		},
-	}
+	}))
 end
 
 ---@param info EnumDeriveInfo
@@ -181,7 +181,7 @@ local function build_meta_file_for_enum(info)
 			parts = astypes,
 		}
 	end
-	return meta_gen {
+	return U.notail(meta_gen({
 		kind = "file",
 		filename = "types." .. name,
 		definition = {
@@ -191,7 +191,7 @@ local function build_meta_file_for_enum(info)
 			methods = methods,
 			constructors = constructors,
 		},
-	}
+	}))
 end
 
 ---@type Deriver

metalanguage-smoketest.lua
@@ -152,7 +152,7 @@ local function val_bind(syntax, env)
 	if not ok then
 		return false, symbol.str
 	end
-	return true, value(nil), env + newenv { [symbol.str] = val }
+	return true, value(nil), env + newenv({ [symbol.str] = val })
 end
 
 local env = newenv {

metalanguage.lua
@@ -1,5 +1,8 @@
 -- SPDX-License-Identifier: Apache-2.0
 -- SPDX-FileCopyrightText: 2025 Fundament Software SPC <https://fundament.software>
+
+local U = require "alicorn-utils"
+
 --[[
 Matcher(userdata : type, results : tuple-desc) : type
 Syntax : type
@@ -221,7 +224,7 @@ end
 ---@return boolean | any, ExternalError | any, ...
 local function augment_error(syntax, reducer_name, ok, err_msg, ...)
 	if not ok then
-		return false, ExternalError.new(err_msg, syntax.start_anchor, reducer_name)
+		return false, U.notail(ExternalError.new(err_msg, syntax.start_anchor, reducer_name))
 	end
 	-- err_msg is the first result arg otherwise
 	return err_msg, ...
@@ -231,7 +234,6 @@ local pdump = require "pretty-printer".s
 -- local function pdump(_)
 -- 	return ""
 -- end
-local U = require "alicorn-utils"
 
 ---@class Reducer
 ---@field wrapper fun(syntax: ConstructedSyntax, matcher: Matcher) : ...
@@ -310,9 +312,9 @@ end
 ---@return boolean
 ---@return string?
 local function SymbolExact(syntax, name)
-	return syntax:match({
+	return U.notail(syntax:match({
 		issymbol(symbolexacthandler),
-	}, failure_handler, name)
+	}, failure_handler, name))
 end
 
 local symbol_exact = reducer(SymbolExact, "symbol exact")
@@ -408,7 +410,7 @@ function ConstructedSyntax:match(matchers, unmatched, extra)
 				if not matcher.handler then
 					print("missing handler for ", matcher.kind, debug.traceback())
 				end
-				return matcher.handler(extra, table.unpack(res, 2, res.n))
+				return U.notail(matcher.handler(extra, table.unpack(res, 2, res.n)))
 			end
 			--print("rejected syntax reduction")
 			lasterr = res[2] --[[@as any]]
@@ -416,7 +418,7 @@ function ConstructedSyntax:match(matchers, unmatched, extra)
 		-- local name = getmetatable(matcher.reducible)
 		-- print("rejected syntax kind", matcher.kind, name)
 	end
-	return unmatched(extra, syntax_error(matchers, self.start_anchor, self.end_anchor, lasterr))
+	return U.notail(unmatched(extra, syntax_error(matchers, self.start_anchor, self.end_anchor, lasterr)))
 end
 
 local constructed_syntax_mt = {
@@ -437,7 +439,7 @@ end
 
 local pair_accepters = {
 	Pair = function(self, matcher, extra)
-		return matcher.handler(extra, self[1], self[2])
+		return U.notail(matcher.handler(extra, self[1], self[2]))
 	end,
 }
 
@@ -447,12 +449,12 @@ local pair_accepters = {
 ---@param b ConstructedSyntax
 ---@return ConstructedSyntax
 local function pair(start_anchor, end_anchor, a, b)
-	return cons_syntax(pair_accepters, start_anchor, end_anchor, a, b)
+	return U.notail(cons_syntax(pair_accepters, start_anchor, end_anchor, a, b))
 end
 
 local symbol_accepters = {
 	Symbol = function(self, matcher, extra)
-		return matcher.handler(extra, self[1])
+		return U.notail(matcher.handler(extra, self[1]))
 	end,
 }
 
@@ -461,12 +463,12 @@ local symbol_accepters = {
 ---@param syntaxsymbol SyntaxSymbol
 ---@return ConstructedSyntax
 local function symbol(start_anchor, end_anchor, syntaxsymbol)
-	return cons_syntax(symbol_accepters, start_anchor, end_anchor, syntaxsymbol)
+	return U.notail(cons_syntax(symbol_accepters, start_anchor, end_anchor, syntaxsymbol))
 end
 
 local value_accepters = {
 	Value = function(self, matcher, extra)
-		return matcher.handler(extra, self[1])
+		return U.notail(matcher.handler(extra, self[1]))
 	end,
 }
 
@@ -481,12 +483,12 @@ local value_accepters = {
 ---@param val SyntaxValue
 ---@return ConstructedSyntax
 local function value(start_anchor, end_anchor, val)
-	return cons_syntax(value_accepters, start_anchor, end_anchor, val)
+	return U.notail(cons_syntax(value_accepters, start_anchor, end_anchor, val))
 end
 
 local nil_accepters = {
 	Nil = function(self, matcher, extra)
-		return matcher.handler(extra)
+		return U.notail(matcher.handler(extra))
 	end,
 }
 
@@ -494,7 +496,7 @@ local nil_accepters = {
 ---@param end_anchor Anchor
 ---@return ConstructedSyntax
 local function new_nilval(start_anchor, end_anchor)
-	return cons_syntax(nil_accepters, start_anchor, end_anchor)
+	return U.notail(cons_syntax(nil_accepters, start_anchor, end_anchor))
 end
 
 ---@param start_anchor Anchor
@@ -504,7 +506,7 @@ end
 ---@return ConstructedSyntax
 local function list(start_anchor, end_anchor, a, ...)
 	if a == nil then
-		return new_nilval(start_anchor, end_anchor)
+		return U.notail(new_nilval(start_anchor, end_anchor))
 	end
 	return pair(start_anchor, end_anchor, a, list(start_anchor, end_anchor, ...))
 end
@@ -693,7 +695,7 @@ local oneof = reducer(
 	---@param ... Matcher
 	---@return ...
 	function(syntax, ...)
-		return syntax:match({ ... }, failure_handler, nil)
+		return U.notail(syntax:match({ ... }, failure_handler, nil))
 	end,
 	"oneof"
 )

modules.lua
@@ -1,5 +1,6 @@
 -- SPDX-License-Identifier: Apache-2.0
 -- SPDX-FileCopyrightText: 2025 Fundament Software SPC <https://fundament.software>
+local U = require "alicorn-utils"
 local types = require "typesystem"
 local evaluator = require "evaluator"
 local environment = require "environment"
@@ -41,27 +42,27 @@ local function new_mod(values)
 end
 
 local function index_mod(mod, name)
-	return mod.values:get(name)
+	return U.notail(mod.values:get(name))
 end
 
 local function use_mod(mod, env)
 	-- print "in use_mod"
 	-- p(mod)
-	return environment.new_env {
+	return U.notail(environment.new_env({
 		locals = env.locals,
 		nonlocals = env.nonlocals:extend(mod.values),
 		carrier = env.carrier,
 		perms = env.perms,
-	}
+	}))
 end
 
 local function open_mod(mod, env)
-	return environment.new_env {
+	return U.notail(environment.new_env({
 		locals = env.locals:extend(mod.values),
 		nonlocals = env.nonlocals,
 		carrier = env.carrier,
 		perms = env.perms,
-	}
+	}))
 end
 
 local function use_mod_op_impl(syntax, env)
@@ -74,7 +75,7 @@ local function use_mod_op_impl(syntax, env)
 	if #modval ~= 1 or modval[1].type ~= module_type then
 		return false, "using syntax should take exactly one argument, a module"
 	end
-	return true, types.unit_val, use_mod(modval[1].val, env)
+	return true, types.unit_val, U.notail(use_mod(modval[1].val, env))
 end
 
 local function open_mod_op_impl(syntax, env)
@@ -87,7 +88,7 @@ local function open_mod_op_impl(syntax, env)
 	if #modval ~= 1 or modval[1].type ~= module_type then
 		return false, "open-mod syntax should take exactly one argument, a module"
 	end
-	return true, types.unit_val, open_mod(modval[1].val, env)
+	return true, types.unit_val, U.notail(open_mod(modval[1].val, env))
 end
 
 local function get_op_impl(syntax, env)
@@ -104,7 +105,7 @@ local function get_op_impl(syntax, env)
 	if not modval.type == module_type then
 		return false, "first argument of module get must be a module"
 	end
-	return true, index_mod(modval.val)
+	return true, U.notail(index_mod(modval.val))
 end
 
 local function mod_op_impl(syntax, env)

nvim-site/queries/lua/tail_calls.scm
@@ -4,16 +4,29 @@
 
 (return_statement
 	(expression_list
-		(function_call
-			name: [
-				(identifier) @_function_name
-				(dot_index_expression
-					field: (identifier) @_function_name
+		[
+			(function_call
+				name: [
+					(identifier) @_function_name
+					(dot_index_expression
+						field: (identifier) @_function_name
+					)
+					(method_index_expression
+						method: (identifier) @_function_name
+					)
+				]
+				(#not-any-of? @_function_name
+					"getmetatable"
+					"notail"
+					"pack"
+					"setmetatable"
+					"unpack"
+					"var_debug"
 				)
-				(method_index_expression)
-				(parenthesized_expression)
-			]
-			(#not-eq? @_function_name "notail")
-		)
+			)
+			(function_call
+				name: (parenthesized_expression)
+			)
+		]
 	)
 ) @tail_call

runtest.lua
@@ -191,7 +191,7 @@ local function load_alc_file(name, env, log)
 
 	log("Parsing code")
 	local ok, code = pcall(function()
-		return format.read(src, name)
+		return U.notail(format.read(src, name))
 	end)
 
 	if not ok then
@@ -316,7 +316,7 @@ local function execute_alc_file(bound_expr, log, env)
 
 	log("Evaluating")
 	local ok, result = pcall(function()
-		return evaluator.evaluate(term, env.typechecking_context.runtime_context, env.typechecking_context)
+		return U.notail(evaluator.evaluate(term, env.typechecking_context.runtime_context, env.typechecking_context))
 	end)
 
 	if not ok then
@@ -334,7 +334,7 @@ local function execute_alc_file(bound_expr, log, env)
 
 	log("Executing")
 	local ok, result_exec = pcall(function()
-		return evaluator.execute_program(result)
+		return U.notail(evaluator.execute_program(result))
 	end)
 
 	if not ok then

terms.lua
@@ -57,12 +57,12 @@ local Metavariable = {}
 
 ---@return stuck_value
 function Metavariable:as_stuck()
-	return stuck_value.free(free.metavariable(self))
+	return U.notail(stuck_value.free(free.metavariable(self)))
 end
 
 ---@return flex_value
 function Metavariable:as_flex()
-	return flex_value.stuck(self:as_stuck())
+	return U.notail(flex_value.stuck(self:as_stuck()))
 end
 
 local metavariable_mt = { __index = Metavariable }
@@ -176,7 +176,7 @@ local StrictRuntimeContext = U.shallow_copy(FlexRuntimeContext)
 ---@return strict_value
 ---@return var_debug?
 function StrictRuntimeContext:get(index)
-	return FlexRuntimeContext.get(self, index):unwrap_strict()
+	return U.notail(FlexRuntimeContext.get(self, index):unwrap_strict())
 end
 
 ---@param v strict_value
@@ -188,7 +188,7 @@ function StrictRuntimeContext:append(v, name, debuginfo)
 		error("StrictRuntimeContext:append v must be a strict_value")
 	end
 	---@type StrictRuntimeContext
-	return FlexRuntimeContext.append(self, flex_value.strict(v), name, debuginfo)
+	return U.notail(FlexRuntimeContext.append(self, flex_value.strict(v), name, debuginfo))
 end
 
 ---@param index integer
@@ -199,7 +199,7 @@ function StrictRuntimeContext:set(index, v)
 		error("StrictRuntimeContext:set v must be a strict_value")
 	end
 	---@type StrictRuntimeContext
-	return FlexRuntimeContext.set(self, index, flex_value.strict(v))
+	return U.notail(FlexRuntimeContext.set(self, index, flex_value.strict(v)))
 end
 
 local strict_runtime_context_mt = {
@@ -527,7 +527,7 @@ end
 
 ---@return integer
 function TypecheckingContext:len()
-	return self.bindings:len()
+	return U.notail(self.bindings:len())
 end
 
 typechecking_context_mt = {
@@ -1163,7 +1163,7 @@ local orig_literal_constructor = typed_term.literal
 local function literal_constructor_check(val)
 	-- FIXME: make sure no placeholders in val
 	verify_placeholder_lite(val, typechecking_context())
-	return orig_literal_constructor(val)
+	return U.notail(orig_literal_constructor(val))
 end
 typed_term.literal = literal_constructor_check
 
@@ -1242,9 +1242,9 @@ local function replace_flex_values(tag, v)
 		error("Unknown tag: " .. tag)
 	elseif v == array(flex_value) then
 		if tag == "strict" then
-			return array(strict_value)
+			return U.notail(array(strict_value))
 		elseif tag == "stuck" then
-			return array(flex_value)
+			return U.notail(array(flex_value))
 		end
 		error("Unknown tag: " .. tag)
 	elseif v == flex_runtime_context_type then
@@ -1563,7 +1563,7 @@ gen.define_multi_enum(
 			if stuck_value.value_check(val) or flex_value.value_check(val) then
 				error("Tried to put flex or stuck value into strict_value.host_value!" .. tostring(val))
 			end
-			return orig_host_value_constructor(val)
+			return U.notail(orig_host_value_constructor(val))
 		end
 		strict_value.host_value = host_value_constructor_check
 		
@@ -1576,7 +1576,7 @@ gen.define_multi_enum(
 				end
 			end
 		
-			return orig_host_tuple_value_constructor(val)
+			return U.notail(orig_host_tuple_value_constructor(val))
 		end
 		strict_value.host_tuple_value = host_tuple_value_constructor_check
 	end
@@ -1624,13 +1624,13 @@ local strict_value_array = array(strict_value)
 ---@param ... flex_value
 ---@return flex_value
 local function tup_val(...)
-	return flex_value.tuple_value(flex_value_array(...))
+	return U.notail(flex_value.tuple_value(flex_value_array(...)))
 end
 
 ---@param ... flex_value
 ---@return flex_value
 local function cons(...)
-	return flex_value.enum_value(DescCons.cons, tup_val(...))
+	return U.notail(flex_value.enum_value(DescCons.cons, tup_val(...)))
 end
 
 local empty = flex_value.enum_value(DescCons.empty, tup_val())
@@ -1657,19 +1657,19 @@ local tristate = gen.declare_enum("tristate", {
 ---@param ... flex_value
 ---@return flex_value
 local function tuple_desc(...)
-	return tuple_desc_inner(empty, ...)
+	return U.notail(tuple_desc_inner(empty, ...))
 end
 
 ---@param ... strict_value
 ---@return strict_value
 local function strict_tup_val(...)
-	return strict_value.tuple_value(strict_value_array(...))
+	return U.notail(strict_value.tuple_value(strict_value_array(...)))
 end
 
 ---@param ... strict_value
 ---@return strict_value
 local function strict_cons(...)
-	return strict_value.enum_value(DescCons.cons, strict_tup_val(...))
+	return U.notail(strict_value.enum_value(DescCons.cons, strict_tup_val(...)))
 end
 
 ---@param a strict_value
@@ -1687,7 +1687,7 @@ end
 ---@param ... strict_value
 ---@return strict_value
 local function strict_tuple_desc(...)
-	return strict_tuple_desc_inner(empty:unwrap_strict(), ...)
+	return U.notail(strict_tuple_desc_inner(empty:unwrap_strict(), ...))
 end
 
 local unit_type = strict_value.tuple_type(empty:unwrap_strict())

commit 51165e334eb81f962903ac2db48109fe9f1a3b6d

terms: fix missing `alicorn-utils` import
Fixes: 67e3cfef9316 ("don't tail call by default (part two)")

terms-gen-meta.lua
@@ -4,6 +4,7 @@ local terms = require "terms"
 local derivers = require "derivers"
 local traits = require "traits"
 local acg = require "libs.abstract-codegen"
+local U = require "alicorn-utils"
 
 -- minimal example:
 -- ---@meta "purity.lua"