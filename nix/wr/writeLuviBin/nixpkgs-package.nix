35bb115ac7d28b5c5b18c33925253a22116b013d
Dusk Banks <me@bb010g.com>
2025-02-14 04:30:06 -0800

    fix Nix source licensing
    some of the licensing for Luvit-related sources will be weird until
    those files are upstreamed back to luvit-nix.

--- nix/wr/writeLuviBin/nixpkgs-package.nix
+++ nix/wr/writeLuviBin/nixpkgs-package.nix
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+# SPDX-FileCopyrightText: 2020 Alex Iverson <alexjiverson@gmail.com>
+# SPDX-FileCopyrightText: 2025 Dusk Banks <me@bb010g.com>
 { ... }:
 { writeLuvi, ... }:
 name: writeLuvi "/bin/${name}"
