9f459dff69efb23fd6e814fb2f556781109f91e2
Erik McClure <erikm@fundament.software>
2025-02-18 20:12:13 -0800

    reuse compliance

--- glsl-print.lua
+++ glsl-print.lua
@@ -1,3 +1,5 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- SPDX-FileCopyrightText: 2025 Fundament Software SPC <https://fundament.software>
 local internals_interface = require "internals-interface"
 internals_interface.glsl_registry = {}
 internals_interface.glsl_print = glsl_print
